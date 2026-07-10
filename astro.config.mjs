5104849fc4d98e5cca3ffdd3e5c90ada4d811401
jonichrishannon-eng <257750484+jonichrishannon-eng@users.noreply.github.com>
2026-07-10 09:03:38 +0200

    feat: implement Pidea component, global styles, and project configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [react(), tailwind()],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  }
 });
